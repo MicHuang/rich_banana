3a5f1539026616ae0e20ba6a7e1329a0fef0a5a2
Mickey Huang <ekey.huang@gmail.com>
2020-05-27 17:56:58 -0400

    fix fetch api name parameter problem

--- acnh/views.py
+++ acnh/views.py
@@ -4,12 +4,14 @@ import requests
 # Create your views here.
 def index(req):
     fish_data = {}
+    name_set = {}
     if 'fish_name' in req.GET:
         fish_name = req.GET['fish_name']
         url = 'http://acnhapi.com/v1/fish/%s' % fish_name
         response = requests.get(url)
         fish_data = response.json()
+        name_set = fish_data['name']
     return render(req, 'acnh/index.html', {
-        'fish_name': fish_name,
+        'fish_name': name_set['name-CNzh'],
         'fish_data': fish_data
         })
